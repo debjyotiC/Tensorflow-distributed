c23bf9cea8abb237dc1a77cd7bd1caffdbba7aa1
Debjyoti Chowdhury <djbabai.debjyoti@gmail.com>
2019-07-27 10:20:37 +0530

    Update cluster-tf.py

--- cluster-tf.py
+++ cluster-tf.py
@@ -12,7 +12,7 @@ b = tf.constant(2.0)
 with tf.device("/job:worker/task:0"):
     x = tf.add(a, b)
     y = tf.multiply(a, b)
-    z = x + y
+    z = tf.add(x, y)
 
 with tf.compat.v1.Session('grpc://X.X.X.X:2222', config=tf.compat.v1.ConfigProto(log_device_placement=True)) as sess:
     print(sess.run(z))
