13ee368c1731cb6e676fd5cc5a9976ac5bb1a9f4
Deb <deb@ubuntu.ubuntu-domain>
2019-07-27 09:01:28 +0530

    added Parameter server code and IP list

---
+++ cluster-tf.py
@@ -0,0 +1,19 @@
+import json
+import tensorflow as tf
+
+with open('cluster_spec/clusterspec.json', 'r') as f:
+    clusterspec = json.load(f)
+
+cluster = tf.train.ClusterSpec(clusterspec)
+
+a = tf.constant(3.0)
+b = tf.constant(2.0)
+
+with tf.device("/job:worker/task:0"):
+    x = tf.add(a, b)
+    y = tf.multiply(a, b)
+    z = x + y
+
+with tf.compat.v1.Session('grpc://192.168.1.2:2222', config=tf.compat.v1.ConfigProto(log_device_placement=True)) as sess:
+    print(sess.run(z))
+
